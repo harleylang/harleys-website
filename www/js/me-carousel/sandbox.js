36d1fb34556f045292b3ddb815d18cd17b2555a7
Harley Lang <harleylang+git@pm.me>
2022-07-23 12:54:16 +0100

    sandbox.js: add: slides with content

--- www/js/me-carousel/sandbox.js
+++ www/js/me-carousel/sandbox.js
@@ -2,21 +2,41 @@
 const parent = document.querySelector("me-carousel");
 
 const children = `
-    <img
-        src="https://dribbble.s3.amazonaws.com/users/322/screenshots/872485/coldchase.jpg"
-        alt="image 1"
-    />
+    <section>
+        <img
+            src="https://dribbble.s3.amazonaws.com/users/322/screenshots/872485/coldchase.jpg"
+            alt="image 1"
+        />
+        <h3>Me image</h3>
+        <p>This is image 1</p>
+    </section>
+    <section>
+        <img
+            src="https://dribbble.s3.amazonaws.com/users/322/screenshots/980517/icehut_sm.jpg"
+            alt="image 2"
+        />
+        <h3>Me image</h3>
+        <p>This is image 2</p>
+    </section>
+    <section>
+        <img
+            src="https://dribbble.s3.amazonaws.com/users/322/screenshots/943660/hq_sm.jpg"
+            alt="image 3"
+        />
+        <h3>Me image</h3>
+        <p>This is image 3</p>
+    </section>
+    <section>
+        <img
+            src="https://dribbble.s3.amazonaws.com/users/322/screenshots/599584/home.jpg"
+            alt="image 4"
+        />
+        <h3>Me image</h3>
+        <p>This is image 4</p>
+    </section>
     <img
         src="https://dribbble.s3.amazonaws.com/users/322/screenshots/980517/icehut_sm.jpg"
-        alt="image 2"
-    />
-    <img
-        src="https://dribbble.s3.amazonaws.com/users/322/screenshots/943660/hq_sm.jpg"
-        alt="image 3"
-    />
-    <img
-        src="https://dribbble.s3.amazonaws.com/users/322/screenshots/599584/home.jpg"
-        alt="image 4"
+        alt="image 5"
     />
 `;
 

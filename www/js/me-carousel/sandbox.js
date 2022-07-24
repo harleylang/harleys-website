4a24e4daf79f40b46fb158220135309b9d17e0e8
Harley Lang (Desktop) <harleylang+git@pm.me>
2022-07-24 20:29:11 +0100

    sandbox.js: add: numbering for headers

--- www/js/me-carousel/sandbox.js
+++ www/js/me-carousel/sandbox.js
@@ -7,7 +7,7 @@ const children = `
             src="https://dribbble.s3.amazonaws.com/users/322/screenshots/872485/coldchase.jpg"
             alt="image 1"
         />
-        <h3>Me image</h3>
+        <h3>Me image 1</h3>
         <p>This is image 1</p>
     </section>
     <section>
@@ -15,7 +15,7 @@ const children = `
             src="https://dribbble.s3.amazonaws.com/users/322/screenshots/980517/icehut_sm.jpg"
             alt="image 2"
         />
-        <h3>Me image</h3>
+        <h3>Me image 2</h3>
         <p>This is image 2</p>
     </section>
     <section>
@@ -23,7 +23,7 @@ const children = `
             src="https://dribbble.s3.amazonaws.com/users/322/screenshots/943660/hq_sm.jpg"
             alt="image 3"
         />
-        <h3>Me image</h3>
+        <h3>Me image 3</h3>
         <p>This is image 3</p>
     </section>
     <section>
@@ -31,7 +31,7 @@ const children = `
             src="https://dribbble.s3.amazonaws.com/users/322/screenshots/599584/home.jpg"
             alt="image 4"
         />
-        <h3>Me image</h3>
+        <h3>Me image 4</h3>
         <p>This is image 4</p>
     </section>
     <img

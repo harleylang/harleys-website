47fae9b6794cbb70ccd3b2ba0d1a3dd7d496b882
Harley Lang (Desktop) <harleylang+git@pm.me>
2022-08-07 12:59:39 +0100

    show-html-comments: add: module for toggling the display of HTML comments

---
+++ www/js/modules/show-html-comments/src/index.ts
@@ -0,0 +1,67 @@
+/**
+ * show-html-comments.mjs
+ * @description
+ * This module will find all elements that have the "htmlCommentToggle"
+ * attribute, attach an event listener that fires on click. The event
+ * will bubble and when caught, will toggle displaying inline HTML
+ * comments within this document.
+ */
+
+// TODO: use DOM mutation observer to remove and re-attach listeners
+
+function dispatchHtmlToggleEvent() {
+  const commentToggleEvent = new CustomEvent("toggleHtmlComments");
+  document.dispatchEvent(commentToggleEvent);
+}
+
+function attachHtmlToggleListeners() {
+  const toggles = document.querySelectorAll("[htmlCommentToggle]");
+  for (let t = 0; t < toggles.length; t += 1) {
+    const toggle = toggles[t];
+    toggle.addEventListener("click", dispatchHtmlToggleEvent);
+  }
+}
+
+function findCommentNodes(el: Document | HTMLElement | ChildNode) {
+  // eslint-disable-next-line no-var
+  var arr: ChildNode[] = [];
+  const nodes = Array.from(el.childNodes);
+  nodes.forEach((node) => {
+    if (node.nodeType === Node.COMMENT_NODE) {
+      arr.push(node);
+    } else {
+      arr.push(...findCommentNodes(node));
+    }
+  });
+  return arr;
+}
+
+function handleHtmlToggleEvents() {
+  const commentNodes = findCommentNodes(document);
+
+  commentNodes.forEach((comment) => {
+    const sibling = (comment as HTMLElement).previousElementSibling;
+    if (sibling) {
+      const isRevealed =
+        typeof (sibling as unknown as HTMLElement).getAttribute(
+          "htmlCommentRevealed"
+        ) === "string";
+      switch (true) {
+        case isRevealed:
+          sibling.remove();
+          break;
+        default:
+          if (comment.parentNode !== null) {
+            const p = document.createElement("p");
+            p.innerHTML = comment.nodeValue ?? "";
+            p.setAttribute("htmlCommentRevealed", "");
+            comment.parentNode.insertBefore(p, comment);
+          }
+          break;
+      }
+    }
+  });
+}
+
+attachHtmlToggleListeners();
+document.addEventListener("toggleHtmlComments", handleHtmlToggleEvents);

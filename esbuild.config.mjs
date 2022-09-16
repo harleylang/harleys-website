460ecbbe0672d97d048dd3e3ef02586922b0c27f
Harley Lang (Desktop) <harleylang+git@pm.me>
2022-09-16 20:09:38 +0100

    esbuild.config.mjs: add: harmonize build sys

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -2,23 +2,10 @@ import esbuild from "esbuild";
 import htmlPlugin from "@chialab/esbuild-plugin-html";
 
 await esbuild.build({
-  entryPoints: ["www/html/index.html"],
+  entryPoints: ["www/html/index.html", "www/html/blog/index.html"],
   entryNames: "[dir]/[name]",
   outdir: "public",
   outbase: "www/html",
-  plugins: [htmlPlugin()],
-  assetNames: "[dir]/[name]",
-  chunkNames: "[dir]/[name]-[hash]",
-  bundle: true,
-  minify: true,
-  write: true,
-});
-
-await esbuild.build({
-  entryPoints: ["www/html/blog/index.html"],
-  entryNames: "[dir]/[name]",
-  outdir: "public/blog",
-  outbase: "www/html/blog",
   plugins: [htmlPlugin()],
   assetNames: "[dir]/[name]",
   chunkNames: "[dir]/[name]-[hash]",

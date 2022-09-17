c17c86d3572af4c1162767d3defc53092d7e3501
Harley Lang (Desktop) <harleylang+git@pm.me>
2022-09-17 13:06:29 +0100

    *: refactor: use rollup for build system

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
+import path from "path";
+
+export default {
+  output: {
+    dir: "public",
+    assetFileNames: (assetInfo) => {
+      let extType = assetInfo.name.split(".").at(1);
+      if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+        extType = "img";
+      }
+      return `${extType}/[name]-[hash][extname]`;
+    },
+    entryFileNames: "js/[name]-[hash].js",
+  },
+  plugins: [
+    html({
+      input: ["./index.html", "./blog/index.html"],
+      rootDir: path.join(process.cwd(), "./www/html"),
+      flattenOutput: false,
+    }),
+  ],
+};

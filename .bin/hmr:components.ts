a55392e4c8ad0241a939557dd054471f0da255f9
Harley Lang (Desktop) <harleylang+git@pm.me>
2022-12-01 21:20:33 +0000

    hmr:components.ts: add: functionlaity to trigger rebuilds of components when their depending css changes

---
+++ .bin/hmr:components.ts
@@ -0,0 +1,24 @@
+import { join } from 'https://deno.land/std@0.119.0/path/win32.ts';
+import esbuildWrapper from './esbuildWrapper.ts';
+
+/**
+ * hmrComponents()
+ * Some components require css styles loaded directly into them.
+ * This is managed by the sass/css style set up in this repo.
+ * In order to trigger a rebuild, this function is attached to the
+ * filewhistle and receives each file change. If the style for
+ * a component is observed, then this function will trigger a
+ * rebuild of the corresponding component automatically.
+ * @param filename
+ */
+export default async function hmrComponents(filename: string) {
+  if (filename.match(/me-(.*)(\.css)/g)) {
+    console.log(filename);
+    const target = filename
+      .replace('/css', '/js')
+      .replace('/dist', '')
+      .replace('.css', '.ts');
+    await esbuildWrapper({ target, outdir: 'www/js/dist' });
+    console.log(`🔔 Whislter rebuild: ${target}`);
+  }
+}

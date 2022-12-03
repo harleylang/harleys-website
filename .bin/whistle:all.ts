280f66efb6d5d56c09d7a28412563e4e906bb668
Harley Lang (Desktop) <harleylang+git@pm.me>
2022-12-03 13:36:16 +0000

    .bin/whistle:scss.ts: fix: file naming

--- .bin/whistle:all.ts
+++ .bin/whistle:all.ts
@@ -2,7 +2,7 @@ import { IWhistleEffect } from './filewhistler.ts';
 
 import whistleComponents from './whistle:components.ts';
 import whistleJs from './whistle:js.ts';
-import whistleScss from './whistle.scss.ts';
+import whistleScss from './whistle:scss.ts';
 import whistleSsg from './whistle:ssg.ts';
 
 const whistles: IWhistleEffect[] = [
